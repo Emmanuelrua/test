160dfdab78a07cabea6c06aee6a4fc1167876545
Ruiz <emmanuelruamontoya@gmail.com>
2026-02-10 15:09:28 -0500

    ClassMethods

--- metodosClass/main.py
+++ metodosClass/main.py
@@ -1,4 +1,24 @@
+class Pizza:
+    def __init__(self,ingredientes : list):
+        self.ingredientes : list = ingredientes
+    
+    @classmethod
+    def crear_hawaiana(cls) -> 'Pizza':
+        return cls(["Queso","Piña","Jamon"])
+    
+    def __repr__(self):
+        return f"PIZZA\n {self.ingredientes}"
+        
+
+
+
 def main():
+    pizza1=Pizza(["chicharron","queso","peperoni"])
+    pizza_hawaiana= Pizza.crear_hawaiana()
+
+    print(pizza1)
+    print(pizza_hawaiana)
+
     print("Hello from metodosclass!")
 
 
